af7af6727c0a6fdd83db4723d5e176805ed925bb
Ty <tytowry@gmail.com>
2015-10-26 22:41:27 -0700

    5 tests 0 failures

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+RSpec.describe User, type: :model do
+  it 'requires a first name' do
+    # your code here
+  end
+  it 'requires a last name' do
+    # your code here
+  end
+  it 'requires a password' do
+    # your code here
+  end
+  it 'accepts properly formatted email addresses' do
+    # your code here
+  end
+  it 'rejects unproperly formatted email addresses' do
+    # your code here
+  end
+end
