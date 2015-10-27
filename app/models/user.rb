1b8e9c05e3c9eee9ed0b87815bb02a71827565ed
Ty <tytowry@gmail.com>
2015-10-26 22:53:59 -0700

    update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
   
   email_regex = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]+)\z/i
 
-    validates :first_name,		:presence 	=> true,
+  validates :first_name,		:presence 	=> true,
   					:length		            => { :maximum => 50 }
   validates :last_name,    :presence   => true,
             :length               => { :maximum => 50 }
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,18 +1,12 @@
 require 'rails_helper'
-RSpec.describe User, type: :model do
-  it 'requires a first name' do
-    # your code here
-  end
-  it 'requires a last name' do
-    # your code here
-  end
-  it 'requires a password' do
-    # your code here
-  end
-  it 'accepts properly formatted email addresses' do
-    # your code here
-  end
-  it 'rejects unproperly formatted email addresses' do
-    # your code here
+RSpec.describe 'listing users' do
+  it 'displays lists of existing users' do
+    user1 = User.create(first_name: 'Dexter', last_name: 'Clark', email: 'dexter@gmail.com', password: 'password')
+    user2 = User.create(first_name: 'Johnny', last_name: 'Chimpo', email: 'mailchimp@gmail.com', password: 'password')
+    user3 = User.create(first_name: 'Tom', last_name: 'Riddle', email: 'voldemort@hogwarts.com', password: 'password')
+    visit '/users'
+    expect(page).to have_text(user1.first_name)
+    expect(page).to have_text(user2.first_name)
+    expect(page).to have_text(user3.first_name)
   end
 end
